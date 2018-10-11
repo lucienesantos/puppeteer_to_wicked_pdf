0e67741197371a45c32b2163e739babe2ef37c5e
Luciene Santos <lucienesantosmc@gmail.com>
2018-10-11 18:27:46 -0300

    Add try catch on puppeteer

--- index.js
+++ index.js
@@ -7,31 +7,52 @@ const args = require('minimist')(process.argv.slice(2));
 const output = fs.createWriteStream(`${__dirname}/log/stdout.log`);
 const errorOutput = fs.createWriteStream(`${__dirname}/log/stderr.log`);
 const logger = new Console({ stdout: output, stderr: errorOutput })
-logger.log(args);
+
+if (args.V) {
+    console.log("0.0.1");
+    return;
+}
 
 (async () => {
-    const HTML = args['_'][0];
-    const PDF_OUT = args['_'][1];
+    logger.log(args);
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    const footerTemplate = args['footer-html'];
-    const footerContent = footerTemplate ? await fse.readFile(footerTemplate.split('file:///')[1], 'utf8') : '';
+    try {
 
-    await page.goto(HTML, { waitUntil: 'networkidle2' });
-    await page.pdf({
-        path: PDF_OUT,
-        format: 'A4',
-        printBackground: true,
-        displayHeaderFooter: true,
-        footerTemplate: footerContent,
-        margin: {
-            top: args['margin-top'] || 0,
-            right: args['margin-right'] || 0,
-            bottom: args['margin-bottom'] || 0,
-            left: args['margin-left'] || 0
+        const HTML = args['_'][0];
+        const PDF_OUT = args['_'][1];
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        const footerTemplate = args['footer-html'];
+        const footerContent = footerTemplate ? await fse.readFile(footerTemplate.split('file:///')[1], 'utf8') : '';
+        const isLandscape = (args['orientation'] != undefined && args['orientation'] == "Landscape");
+        await page.goto(HTML, { waitUntil: 'networkidle2'});
+        await page.emulateMedia("screen");
+
+        if (isLandscape ){
+            await page.setViewport({ width: 842, height: 595 });
         }
-    });
 
+        await page.pdf({
+            path: PDF_OUT,
+            format: 'A4',
+            printBackground: true,
+            displayHeaderFooter: true,
+            landscape: isLandscape,
+            footerTemplate: footerContent,
+            headerTemplate: "<p><p>",
+            margin: {
+                top: args['margin-top'] || 0,
+                right: args['margin-right'] || 0,
+                bottom: args['margin-bottom'] || 0,
+                left: args['margin-left'] || 0
+            }
+        });
     await browser.close();
+
+    } catch(e) {
+        logger.log(e)
+        process.exit(1);
+   }
+
 })();
